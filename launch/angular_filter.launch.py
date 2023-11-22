6ad490c806790fbeb958bf95dd15c10916cf2cbc
lucianoperetti <lucianoperetti.dte@gmail.com>
2023-11-22 12:56:36 -0300

    Agrega parametros para levantar el sistema

---
+++ launch/angular_filter.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="laser_filters",
+            executable="scan_to_scan_filter_chain",
+            parameters=[
+                PathJoinSubstitution([
+                    get_package_share_directory("laser_filters"),
+                    "launch", "angular_filter.yaml",
+                ])],
+            remappings=[
+                ('/scan','/scan_raw'),
+                ('/scan_filtered','/scan'),
+            ]
+        )
+    ])
