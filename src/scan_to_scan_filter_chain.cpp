70b9a017eec5ed0c690d4acbf1a6de3e6bed474d
apeliedev <info@apelie.com>
2024-04-23 13:22:56 -0300

    Ajusta el tamaño del buffer de transformadas

--- src/scan_to_scan_filter_chain.cpp
+++ src/scan_to_scan_filter_chain.cpp
@@ -65,7 +65,7 @@ public:
   ScanToScanFilterChain(rclcpp::Node::SharedPtr node)
       : nh_(node),
         tf_(NULL),
-        buffer_(nh_->get_clock()),
+        buffer_(nh_->get_clock(), std::chrono::seconds(30)),
         scan_sub_(nh_, "scan", rmw_qos_profile_sensor_data),
         tf_filter_(NULL),
         filter_chain_("sensor_msgs::msg::LaserScan")
@@ -80,9 +80,9 @@ public:
       nh_->get_parameter_or("tf_message_filter_tolerance", tf_filter_tolerance_, 0.03);
 
       tf_.reset(new tf2_ros::TransformListener(buffer_));
-      tf_filter_.reset(new tf2_ros::MessageFilter<sensor_msgs::msg::LaserScan>(scan_sub_, buffer_, "", 50, nh_));
-      tf_filter_->setTargetFrame(tf_message_filter_target_frame);
+      tf_filter_.reset(new tf2_ros::MessageFilter<sensor_msgs::msg::LaserScan>(scan_sub_, buffer_, tf_message_filter_target_frame, 50, nh_));
       tf_filter_->setTolerance(std::chrono::duration<double>(tf_filter_tolerance_));
+
 
       // Setup tf::MessageFilter generates callback
       tf_filter_->registerCallback(std::bind(&ScanToScanFilterChain::callback, this, std::placeholders::_1));
